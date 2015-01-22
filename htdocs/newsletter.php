d7ebea39e76475125b08824a38f9d938462fddec
MikeAguilera <michael.aguilera@gmail.com>
2015-01-22 11:32:41 -0800

    converted to php
    converted project to php and updated index with header include

--- htdocs/newsletter.php
+++ htdocs/newsletter.php
@@ -48,6 +48,12 @@
       <h3>Get monthly adoption news updates!</h3>
       <p>We will be your source for adoption information</p>
     </div>  
+        <!--Newsletter Sign-Up Form Begin-->
+        <!--Newsletter Sign-Up Form End-->
+
+
+
+
     <!-- End body -->
    <!-- Footer -->
         <div class="container">
