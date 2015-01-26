edc585713750dbfa1fede97a7515f9f36af1c14e
MikeAguilera <michael.aguilera@gmail.com>
2015-01-26 08:43:07 -0800

    reverted back to lingala app
    copied an pasted from dev app

---
+++ js/slickQuiz-config.js
@@ -0,0 +1,72 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Test Your Knowledge of Lingala!!",
+        "main":    "<p>TLingala (Ngala) is a Bantu language spoken throughout the northwestern part of the Democratic Republic of the Congo and a large part of the Republic of the Congo, as well as to some degree in Angola and the Central African Republic. It has over 10 million speakers.</p>",
+        "results": "<h5>Learn More</h5><p>Click Here to go to the Lingala Wikipedia page</p>",
+        "level1":  "Jeopardy Ready",
+        "level2":  "Jeopardy Contender",
+        "level3":  "Jeopardy Amateur",
+        "level4":  "Jeopardy Newb",
+        "level5":  "Stay in school, kid..." // no comma here
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "What number is the Lingala phrase for apple?",
+            "a": [
+                {"option": "Mbuma ya pomme",      "correct": false},
+                {"option": "Mbuma ya lait",     "correct": false},
+                {"option": "Mbuma ya fromage",      "correct": true},
+                {"option": "Mbuma ya manzana",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
+            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "What number is the Lingala phrase for Baby?",
+            "a": [
+                {"option": "Bacon and eggs",               "correct": false},
+                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
+                {"option": "Leftover pizza",               "correct": false},
+                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+            ],
+            "select_any": true,
+            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
+            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+            "q": "What number is the Lingala phrase for apple?",
+            "a": [
+                {"option": "Planet Earth",           "correct": true},
+                {"option": "Pluto",                  "correct": false},
+                {"option": "At a computing device",  "correct": true},
+                {"option": "The Milky Way",          "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
+            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+        },
+        { // Question 4
+            "q": "What number is the Lingala phrase for apple?",
+            "a": [
+                {"option": "149",    "correct": false},
+                {"option": "32",     "correct": true},
+                {"option": "3",      "correct": false},
+                {"option": "1291",   "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
+            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "What number is the Lingala phrase for apple?",
+            "a": [
+                {"option": "Yes",    "correct": true},
+                {"option": "No",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
+            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+        } // no comma here
+    ]
+};
