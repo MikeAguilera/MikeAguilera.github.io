3b831dbea7055f7497ce609f5ba4b7b71a28c42e
MikeAguilera <michael.aguilera@gmail.com>
2015-01-26 21:25:04 -0800

    converting to php
    added header.php and updated index.php to include header.

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
 
 <!--From Lingala App Begin-->
-    <title>Learn Lingala</title>
+    <title><?php echo $pageTitle; ?></title>
 
         <!--<link href="css/reset.css" media="screen" rel="stylesheet" type="text/css">-->
         <link href="css/slickQuiz.css" media="screen" rel="stylesheet" type="text/css">
@@ -89,54 +89,3 @@
             </div>
             <!-- /.navbar-collapse -->
         </nav>
-
-
-<div id="page-wrapper">
-
-    <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Welcome <small> to Learn Lingala!</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                <i class="fa fa-home"></i> Home
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
-
-<div class="jumbotron">
-    <h1>Test Your Knowledge of Lingala!!</h1>
-    <p>Lingala (Ngala) is a Bantu language spoken throughout the northwestern part of the Democratic Republic of the Congo and a large part of the Republic of the Congo, as well as to some degree in Angola and the Central African Republic. It has over 10 million speakers.</p>
-    <a href="basic.html" class="btn btn-success">Basic Test</a>
-</div>
-
-</div>
-</div>
-
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
-
-    <script src="js/jquery.js"></script>
-    <script src="js/slickQuiz-config.js"></script>
-    <script src="js/slickQuiz.js"></script>
-    <script src="js/master.js"></script>
-    
-    </body>
-
-
-</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+<!--Header Begin-->
+<?php 
+$pageTitle = "Learn Lingala";
+include 'inc/header.php'; ?>
+<!--Header End-->
+
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome <small> to Learn Lingala!</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li class="active">
+                                <i class="fa fa-home"></i> Home
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+<div class="jumbotron">
+    <h1>Test Your Knowledge of Lingala!!</h1>
+    <p>Lingala (Ngala) is a Bantu language spoken throughout the northwestern part of the Democratic Republic of the Congo and a large part of the Republic of the Congo, as well as to some degree in Angola and the Central African Republic. It has over 10 million speakers.</p>
+    <a href="basic.html" class="btn btn-success">Basic Test</a>
+</div>
+
+</div>
+</div>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/slickQuiz-config.js"></script>
+    <script src="js/slickQuiz.js"></script>
+    <script src="js/master.js"></script>
+    
+    </body>
+
+
+</html>
